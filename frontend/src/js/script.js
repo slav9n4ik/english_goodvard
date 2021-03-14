2fc83e08f237e484235a00603d1f612fcc88df1b
slav9n4ik <slava.tresnevv@gmail.com>
2021-03-14 11:06:02 +0300

    add courses mobil version

--- frontend/src/js/script.js
+++ frontend/src/js/script.js
@@ -8,3 +8,59 @@ menuButton.addEventListener("click", () => {
         nav.classList.add('navigation-open');
     }
 });
+
+
+/* Course Carousel */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  if (window.screen.width <= 700) {
+    var i;
+    var slides = document.getElementsByClassName("course-item");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "flex";
+    dots[slideIndex-1].className += " active";
+  } 
+}
+
+
+// Course more info
+function showMoreInfo(n) {
+  let activeItems = document.getElementsByClassName("course-item-active");
+  let display = activeItems[n-1].style.display;
+  if (display === 'none' || display === undefined || display === '') {
+    activeItems[n-1].style.display = 'block';
+  } else {
+    activeItems[n-1].style.display = 'none';
+  }
+
+  let items = document.getElementsByClassName("course-item");
+  for (i = 0; i < items.length; i++) {
+    if (i != (n - 1)) {
+      console.log(items[i])
+      items[i].style.maxHeight = '250px';
+    } else {
+      items[i].style.maxHeight = 'none';
+    }
+  }
+ }
