ca93d8da0d925c39ad48ffe5004e18c931fc3ee2
slav9n4ik <slava.tresnevv@gmail.com>
2021-03-28 13:59:34 +0300

    finish course block

--- frontend/src/js/script.js
+++ frontend/src/js/script.js
@@ -9,7 +9,6 @@ menuButton.addEventListener("click", () => {
     }
 });
 
-
 /* Course Carousel */
 var slideIndex = 1;
 showSlides(slideIndex);
@@ -25,15 +24,15 @@ function currentSlide(n) {
 }
 
 // mobile course slider
-function showSlides(n) {
+function showSlides(n) {  
   if (window.screen.width <= 700) {
     let containers = document.getElementsByClassName("courses-container");
     let slides = document.getElementsByClassName("course-item");
     let activeSlides = document.getElementsByClassName("course-item-active");
     let dots = document.getElementsByClassName("dot");
 
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
+    if (n > slides.length) {slideIndex = 1; n = 1;}
+    if (n < 1) {slideIndex = slides.length; n = slides.length;}
 
     if (slideIndex === 1 || slideIndex === 2) {
       containers[0].style.display = "flex";
@@ -47,36 +46,47 @@ function showSlides(n) {
         slides[i].style.display = "none";
         activeSlides[i].style.display = "none";
     }
+
     for (let i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex-1].style.display = "flex";
-    activeSlides[slideIndex-1].style.display = "flex";
-    dots[slideIndex-1].className += " active";
+
+    slides[n - 1].style.display = "flex";
+    activeSlides[n - 1].style.display = "flex";
+    dots[n - 1].className += " active";
   } 
 }
 
 var arrows = document.getElementsByClassName("more-course-info-btn");
 var activeItems = document.getElementsByClassName("course-item-active");
 var items = document.getElementsByClassName("course-item");
+var offsetHeightItem = items[0].offsetHeight;
 
 // Course desktop more info
 function showMoreInfo(itemIndex) {
+  if (window.screen.width > 700) {
     for (let i = 0; i < items.length; i++) {
-      (i === itemIndex) ? 
-          items[i].className.includes("course-active") ? closeCourseBlock(i) : showCourseBlock(i)
-          : closeCourseBlock(i);
+      if (i === itemIndex) {
+          items[i].className.includes("course-active") ? 
+            closeCourseBlock(i, offsetHeightItem) : showCourseBlock(i, offsetHeightItem)
+      } else {
+        closeCourseBlock(i, offsetHeightItem);
+      } 
     }
+  }
  }
 
-function showCourseBlock(i) {
+function showCourseBlock(i, offsetHeight) {
     items[i].className += " course-active";
+    items[i].style.height = offsetHeight * 1.1 + 'px';
     activeItems[i].style.display = 'flex';
+    activeItems[i].style.marginTop = offsetHeight * -0.05  + 'px';
     arrows[i].style.display = 'none';
  }
 
-function closeCourseBlock(i) {
+function closeCourseBlock(i, offsetHeight) {
     items[i].className = items[i].className.replace(" course-active", "");
+    items[i].style.height = offsetHeight + "px";
     activeItems[i].style.display = 'none';
     arrows[i].style.display = 'block';
 }
