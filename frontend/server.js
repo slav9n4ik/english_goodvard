d1334f720ab6be6c83ec53f9db24c10b2d6e301a
slav9n4ik <slava.tresnevv@gmail.com>
2021-11-08 22:11:22 +0300

    bugfix

--- frontend/server.js
+++ frontend/server.js
@@ -6,15 +6,10 @@ app.use(express.static('src'));
 
 router.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/src/index.html'));
-    //__dirname : It will resolve to your project folder.
 });
 
-router.get('/about',function(req,res){
-    res.sendFile(path.join(__dirname+'/about.html'));
-});
-
-router.get('/sitemap',function(req,res){
-    res.sendFile(path.join(__dirname+'/sitemap.html'));
+router.get('/robots.txt',function(req,res){
+    res.sendFile(path.join(__dirname+'/src/robots.txt'));
 });
 
 //add the router
